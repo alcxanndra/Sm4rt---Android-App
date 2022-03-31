a29b0c3918e9c059b386202a37652d224ba7e9af
Alexandra <alexandracaldaruse@gmail.com>
2022-03-31 20:06:41 +0300

    Local notification with deep link

--- app/src/main/java/com/example/sm4rt/activity/MainActivity.java
+++ app/src/main/java/com/example/sm4rt/activity/MainActivity.java
@@ -3,23 +3,18 @@ package com.example.sm4rt.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
-import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
-import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.sm4rt.NotificationActivity;
 import com.example.sm4rt.R;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,24 +29,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
-        mBuilder.setSmallIcon(R.drawable.ic_launcher_background);
-        mBuilder.setContentTitle("Notification Alert, Click Me!");
-        mBuilder.setContentText("Hi, This is Android Notification Detail!");
-
-        Intent resultIntent = new Intent(this, NotificationActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(NotificationActivity.class);
-
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setContentIntent(resultPendingIntent);
-
         myNotifyMgr = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O &&
-                myNotifyMgr.getNotificationChannel(MYCHANNEL)==null)
-        { myNotifyMgr.createNotificationChannel(new NotificationChannel(MYCHANNEL,
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O &&
+                myNotifyMgr.getNotificationChannel(MYCHANNEL) == null) {
+            myNotifyMgr.createNotificationChannel(new NotificationChannel(MYCHANNEL,
                 "My Channel", NotificationManager.IMPORTANCE_DEFAULT));
         }
 
@@ -70,13 +51,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void raiseNotification(View view) {
-        Intent myIntent = new Intent(this, NotificationActivity.class);
+        String deeplink = "sm4rt.me://discover";
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(deeplink));
+
+//        Intent myIntent = new Intent(this, RandomQuestionActivity.class);
         PendingIntent myPendingIntent = PendingIntent.getActivity(MainActivity.this, 1, myIntent, 0);
         NotificationCompat.Builder myNotifyBuilder = new NotificationCompat.Builder(MainActivity.this, MYCHANNEL);
         myNotifyBuilder.setAutoCancel(false);
-        myNotifyBuilder.setTicker("Here is your ticker text");
+        myNotifyBuilder.setTicker("Remember to be Sm4rt!");
         myNotifyBuilder.setContentTitle("Remember to be Sm4rt!");
-        myNotifyBuilder.setContentText("Learn a quick fact today in less than a minute to train your brain!");
+//        myNotifyBuilder.setContentText("Learn a quick fact today in less than a minute to train your brain!");
+        myNotifyBuilder.setContentText("Learn a quick fact today in less than a minute to train your brain!\n" +  deeplink);
+        myNotifyBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                .bigText("Learn a quick fact today in less than a minute to train your brain!\n" +  deeplink));
         myNotifyBuilder.setSmallIcon(R.drawable.ic_bulb);
         myNotifyBuilder.setContentIntent(myPendingIntent);
         myNotifyBuilder.build();
---
+++ app/src/main/java/com/example/sm4rt/activity/RandomQuestionActivity.java
@@ -0,0 +1,166 @@
+package com.example.sm4rt.activity;
+
+import static com.example.sm4rt.fragment.QuestionListFragment.QUESTION;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.sm4rt.R;
+import com.example.sm4rt.fragment.QuestionFragment;
+import com.example.sm4rt.model.QuestionModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomQuestionActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_question);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.getMenu().getItem(2).setChecked(true);
+
+
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(QUESTION, getRandomQuestion());
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        QuestionFragment secondFragment = new QuestionFragment();
+        secondFragment.setArguments(bundle);
+        fragmentTransaction.replace(R.id.fragment_container, secondFragment)
+                .addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private QuestionModel getRandomQuestion() {
+        List<QuestionModel> questionList = initializeQuestionList();
+        return questionList.get((int) (Math.random() * questionList.size()));
+    }
+
+    // Retrieving the url to share
+    private Uri getImageToShare(Bitmap bitmap) {
+        File imagefolder = new File(getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imagefolder.mkdirs();
+            File file = new File(imagefolder, "shared_image.png");
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+            outputStream.flush();
+            outputStream.close();
+            uri = FileProvider.getUriForFile(this, "com.example.sm4rt.shareimage.fileprovider", file);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return uri;
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("questions.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+    private List<QuestionModel> initializeQuestionList(){
+        List<QuestionModel> questionList = new ArrayList<>();
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            JSONArray array = obj.getJSONArray("questions");
+
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject questionJson = array.getJSONObject(i);
+                String questionTitle = questionJson.getString("title");
+                String questionTopic = questionJson.getString("topic");
+                String questionAnswer = questionJson.getString("answer");
+
+                questionList.add(new QuestionModel(questionTitle, questionTopic, questionAnswer));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return questionList;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.share:
+                bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                String shareText = "https://sm4rt.com/download-app";
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+                Context c = bottomNavigationView.getContext();
+                Drawable d = c.getResources().getDrawable(c.getResources().getIdentifier("logo", "drawable", c.getPackageName()));
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) d;
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                Uri uri = getImageToShare(bitmap);
+
+                shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+                shareIntent.putExtra(Intent.EXTRA_TITLE, "Dear friend, join me on Sm4rt!");
+                shareIntent.setData(uri);
+                shareIntent.setType("image/*");
+                shareIntent.setClipData(ClipData.newRawUri("", uri));
+                shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                startActivity(Intent.createChooser(shareIntent, null));
+                return true;
+
+            case R.id.topics:
+                Intent topicIntent = new Intent(getApplicationContext(), TopicsPage.class);
+                startActivity(topicIntent);
+                return true;
+
+            case R.id.facts:
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(QUESTION, getRandomQuestion());
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                QuestionFragment secondFragment = new QuestionFragment();
+                secondFragment.setArguments(bundle);
+                fragmentTransaction.replace(R.id.fragment_container, secondFragment)
+                        .addToBackStack(null);
+                fragmentTransaction.commit();
+                return true;
+        }
+        return false;
+    }
+}
